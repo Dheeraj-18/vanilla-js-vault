c723b544a450d98847e7fa9a3de1dcc3ab0241bf
think2code <dheerajsaraswat03@gmail.com>
2025-07-13 21:44:28 +0530

    promises- fetch api and promises

--- 73_into-to-fetch-api/script.js
+++ 73_into-to-fetch-api/script.js
@@ -1,5 +1,40 @@
-fetch('https://6wrlmkp9u2.execute-api.us-east-1.amazonaws.com/?sleep=2000')
-.then(response => response.json())
-.then(data => {
-  console.log(data);
+//                                               fetch() API Notes Summary
+//                                     _______________________________________________
+
+// 1. fetch('') return a promise object
+// 2. whenever we request using fetch APi so on the first time it not return a data in promiseResult it return a response object ,on that
+//    we do response.json()  then it also return a promise object with promiseResult is our data
+// 3. In the response object there is headers i.e when the promise resolve then header appears in the response object
+// 3. by default fetch('') send the GET request
+// 4. for other method fetch take a second argument i.e { method : "GET"}
+// 5. we use any method other than GET POST ... but they must be supported by Backend but we generally use std methods GET POST PUT DELETE..
+// 6. with the POST request we also send JSON data
+// 7. when we send POST request we require to set some headers content they are optional
+// 8. BUT when we send POST we set the body content bascically send data in JSON to the backend
+// 9. **TO see and practice use dummyJSON website
+// 10.
+//  ________________________________________________________________________________________________________________________________________
+
+// fetch('https://dummyjson.com/products', { method: 'GET' })
+//   .then((res) => res.json()) // implicit return in arrow fun
+//   .then((data) => {
+//     console.log(data)
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+fetch('https://dummyjson.com/products/add', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    title: 'BMW Pencil',
+    category: 'Stationary',
+    /* other product data */
+  }),
 })
+  .then((res) => res.json())
+  .then(console.log)
+  .catch((err) => {
+    console.log(err)
+  })
