18d4a078d915e9efba6feccb77897fb968f63920
think2code <dheerajsaraswat03@gmail.com>
2025-07-14 06:37:14 +0530

    comment- notes of promise with fetch

--- 73_into-to-fetch-api/script.js
+++ 73_into-to-fetch-api/script.js
@@ -2,8 +2,8 @@
 //                                     _______________________________________________
 
 // 1. fetch('') return a promise object
-// 2. whenever we request using fetch APi so on the first time it not return a data in promiseResult it return a response object ,on that
-//    we do response.json()  then it also return a promise object with promiseResult is our data
+// 2. whenever we request using fetch APi so on the first time it not return a data in promiseResult it return a response object and the
+//   data are inside the body : ReadableStream so ,on that we do response.json()  then it also return a promise object with promiseResult is our data
 // 3. In the response object there is headers i.e when the promise resolve then header appears in the response object
 // 3. by default fetch('') send the GET request
 // 4. for other method fetch take a second argument i.e { method : "GET"}
@@ -12,8 +12,20 @@
 // 7. when we send POST request we require to set some headers content they are optional
 // 8. BUT when we send POST we set the body content bascically send data in JSON to the backend
 // 9. **TO see and practice use dummyJSON website
-// 10.
-//  ________________________________________________________________________________________________________________________________________
+// 10. Javascript give a guarantees with the promise object that request of promise resolve only at ones that means promise object 
+//     have three state <pending> , <fulfilled> , <rejected> so request is just only ones the state change ony onces at a time and also 
+//     that Promise comes with the feature that this is immutable object so we not worry about the data 
+//        a) promise object are immutable 
+//        b) promise just resolve only at once
+// 
+//                               <<<<<<<<     ALL Definition of Promises       >>>>>>>>
+
+// 1. Promise object is a placeholder which will be filled with a later value after completion the async operation
+// 2 ** Mdn web docs (definition)
+//    => Promise is a Object representing the eventual completion or failure of an asynchronous operation and its resulting value. 
+
+
+//  _____________________________________________________________________________________________________________________________________
 
 // fetch('https://dummyjson.com/products', { method: 'GET' })
 //   .then((res) => res.json()) // implicit return in arrow fun
