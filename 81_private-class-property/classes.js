499f1fc9355190a54963a14839adbdea02a264d6
think2code <dheerajsaraswat03@gmail.com>
2025-07-30 15:45:51 +0530

    doc - add some comment

--- 81_private-class-property/classes.js
+++ 81_private-class-property/classes.js
@@ -18,6 +18,7 @@
 //            also but by using # it becomes private and not go in prototype properties object Now, if we access function outside the class
 //            it throw an error
 //                > If we want to access methods inside the class use this keyword eg. this.#getFullName()
+//          > When we make any method in the calls private it not go into the prototype it directly put onto the class 
 
 // NOTE-5: Can we make property private directly inside the object using # ?
 //          > we not using # directly inside the object before property name it throw an error instead of these we use # inside the string .
@@ -27,36 +28,37 @@
 
 // NOTE-6: WE NOT ACCESS PRIVATE PROPERTY OF CLASS BY ANY OF THE METHOD LIKE IN OBJECT. 
 
-// class CreateUser {
-//   #age
-//   constructor(firstName, lastName, age) {
-//     this.firstName = firstName
-//     this.lastName = lastName
-//     this.#age = age
-//   }
+class CreateUser {
+  #age
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.#age = age
+  }
 
-//   #hi = 'Hello world ' // hardcode property in the returning object , make private using #before there name
+  #hi = 'Hello world ' // hardcode property in the returning object , make private using #before there name
 
-//   getBirthYear() {
-//     console.log(this.#hi)
-//     console.log(this.#getFullName())
-//     return new Date().getFullYear() - this.#age
-//   }
-//   #getFullName() {
-//     return this.firstName + ' ' + this.lastName
-//   }
-// }
+  getBirthYear() {
+    console.log(this.#hi)
+    console.log(this.#getFullName())
+    return new Date().getFullYear() - this.#age
+  }
+  #getFullName() {
+    return this.firstName + ' ' + this.lastName
+  }
+}
 
-// const user1 = new CreateUser('Aman', 'Mishra', 32)
-// const user2 = new CreateUser('Anurag', 'Singh', 72)
+const user1 = new CreateUser('Aman', 'Mishra', 32)
+const user2 = new CreateUser('Anurag', 'Singh', 72)
+// console.log(user1.#age);           // this line through error bcz we not able to access private property outside the class 
 
 // user1.getBirthYear()
 // user2.getBirthYear()
 
-const a = {
-  name: 'shriKant',
-  '#age': 22,
-}
+// const a = {
+//   name: 'shriKant',
+//   '#age': 22,
+// }
 
 // a.age       //  output -> undefined 
 // a.#age      //  output -> error
