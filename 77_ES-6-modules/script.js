3187f6ecce7694b3e202be1d3b5766cbf92bd4b4
think2code <dheerajsaraswat03@gmail.com>
2025-09-18 11:38:02 +0530

    doc - some notes changes in ES6 modules topic

--- 77_ES-6-modules/script.js
+++ 77_ES-6-modules/script.js
@@ -1,8 +1,8 @@
 //                   Summary Notes
 
 // NOTE: before using the ES6 import and export facility must set the attribute on script take type = "module"
-// 1. In the early in js there no facility to split our code or export and import SO , at that time we use Common JS for import our code
-//     using require syntax use in Node js as well
+// 1. In the early in js there no facility to split our code or export and import SO , at that time we use "Common JS" for import our code
+//     using "require" syntax use in Node js as well
 // 2. In ES6 there is facility to introduce i.e import and export fo js file in our main file or we split our code and use by export and
 //    import
 // 3. In import syntax { , } the variable name is case sensitive i.e whatever we use in export syntax file same use in import (case sensitive )
@@ -10,6 +10,8 @@
 //     eg. export { myName as MyNames} (i.e convention is named export )
 // 4. same way of changing the name using 'as' keyword in import also we do it
 // 5. we mostly renaming at the   time of import usually
+//            _____________________________________
+
 // 6. another convention of export file using default i.e default export previous one we learn i.e Named export
 // 7. with that default export syntax also change in import syntax corresponding to the default export and i.e in import not use curlyBraces{}
 // 8. while we use default syntax there is facility come with that i.e in import we reaming whatever we want without using 'as' keyword
@@ -19,6 +21,9 @@
 //11. in import we write for both default and names export using , i.e
 //    eg. import userData , {myName} from './userData.js'
 // Inside the curlyBraces import named export and outside using comma import default export
+
+//           _______________________________________
+
 //12. for name export we directly export inline with the variable declaration  i.e
 //       eg. export const userData = []
 //13. But we don't use export default inline with any variable declaration (throw error)
@@ -26,12 +31,17 @@
 //      Yes, in JavaScript, you cannot use export default more than once in a single file. This is because:
 //      ✅ export default defines the default export for a module.
 //      A module (file) can only have one default export.
+
+//         __________________________________________
+
 //15. we also use import * as  anyVariableName form './userData.js'
 //      this give a module object on that all data .
 //   using this syntax * we can export entire but for the default we access this by using anyVariableName.default()
 // when we set type = "module" so we can import and export variable but not accessible globally form console .
 // in is in module scope
 // 16_IMP. when we set type = 'module' then defer is automatic set no need to set defer explicitly
+
+//          --------------------
 //
 // Note-1 when we declare any variable without using let var const it directly go into global window object and
 //        access globally even if we declare inside the function without using let var cost then is also attach to the global
